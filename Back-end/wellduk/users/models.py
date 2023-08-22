1b5705e55eaa8175bf938b182f3dc66fe77a3e29
Jaeyoung <tjy0711@naver.com>
2023-08-22 22:52:57 +0900

    models.py update

--- Back-end/wellduk/users/models.py
+++ Back-end/wellduk/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.EmailField(verbose_name='이메일', unique=True)
+    name = models.CharField(verbose_name='이름', max_length=10, null=False)
+    phone = models.CharField(verbose_name='연락처', max_length=11, null=True, blank=True)
+
+    class Meta:
+        verbose_name = '유저'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.email

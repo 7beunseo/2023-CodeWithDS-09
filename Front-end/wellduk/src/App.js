99cb4cb421c931dec493d1eeaf618f5e5fb8256c
twosky0202 <leehaneul0202@naver.com>
2023-08-22 22:17:55 +0900

    build: react 초기 설정

--- Front-end/wellduk/src/App.js
+++ Front-end/wellduk/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './styles/global'
+import { RouterProvider } from 'react-router-dom'
+import theme from './styles/theme'
+import router from './routes/router'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<GlobalStyles />
+			<RouterProvider router={router} />
+		</ThemeProvider>
+	)
 }
 
-export default App;
+export default App

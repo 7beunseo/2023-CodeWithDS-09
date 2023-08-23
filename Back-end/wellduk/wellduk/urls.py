be7c4a56517cd276ab3f45d492d9fb12fff7d3d7
7beunseo <kimes0403@gmail.com>
2023-08-23 13:45:50 +0900

    feat: Add handover

--- Back-end/wellduk/wellduk/urls.py
+++ Back-end/wellduk/wellduk/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('raon/',include('raons.urls')),
     path('free-communication/',include('freecommunication.urls')),
+    path('hand-over/',include('freecommunication.urls')),
+    
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

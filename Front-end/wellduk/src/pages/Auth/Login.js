f0b055f5c8303a6ad391638c86b6b54fbf848202
twosky0202 <leehaneul0202@naver.com>
2023-08-23 16:23:08 +0900

    style: 로그인, 회원가입 페이지

--- Front-end/wellduk/src/pages/Auth/Login.js
+++ Front-end/wellduk/src/pages/Auth/Login.js
@@ -1,4 +1,58 @@
+import { styled } from 'styled-components'
+import { marginAuto } from '../../styles/common'
+import Input from '../../components/Input/Input'
+import Button from '../../components/Button/Button'
+import { useNavigate } from 'react-router-dom'
+
 function Login() {
-	return <div></div>
+	const navigate = useNavigate()
+	return (
+		<S.Wrapper>
+			<S.Title>로그인</S.Title>
+			<S.Form>
+				<S.Container>
+					<label>아이디</label>
+					<Input />
+				</S.Container>
+				<S.Container>
+					<label>비밀번호</label>
+					<Input />
+				</S.Container>
+				<div onClick={() => navigate('/signup')}>회원가입</div>
+				<Button>로그인</Button>
+			</S.Form>
+		</S.Wrapper>
+	)
 }
 export default Login
+
+const Wrapper = styled.div`
+	${marginAuto}
+	text-align: center;
+`
+
+const Title = styled.h2`
+	padding: 32px;
+`
+
+const Form = styled.form`
+	width: 60%;
+	padding: 20px 0;
+	${marginAuto}
+`
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	align-content: center;
+	flex-wrap: wrap;
+	margin-bottom: 40px;
+`
+
+const S = {
+	Wrapper,
+	Title,
+	Form,
+	Container,
+}
--- Front-end/wellduk/src/pages/Auth/SignUp.js
+++ Front-end/wellduk/src/pages/Auth/SignUp.js
@@ -1,4 +1,63 @@
+import { styled } from 'styled-components'
+import { marginAuto } from '../../styles/common'
+import Input from '../../components/Input/Input'
+import Button from '../../components/Button/Button'
+
 function SignUp() {
-	return <div></div>
+	return (
+		<S.Wrapper>
+			<S.Title>회원가입</S.Title>
+			<S.Form>
+				<S.Container>
+					<label>아이디</label>
+					<Input />
+				</S.Container>
+				<S.Container>
+					<label>비밀번호</label>
+					<Input />
+				</S.Container>
+				<S.Container>
+					<label>비밀번호 확인</label>
+					<Input />
+				</S.Container>
+				<S.Container>
+					<label>닉네임</label>
+					<Input />
+				</S.Container>
+				<Button>회원가입</Button>
+			</S.Form>
+		</S.Wrapper>
+	)
 }
 export default SignUp
+
+const Wrapper = styled.div`
+	${marginAuto}
+	text-align: center;
+`
+
+const Title = styled.h2`
+	padding: 32px;
+`
+
+const Form = styled.form`
+	width: 60%;
+	padding: 20px 0;
+	${marginAuto}
+`
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	align-content: center;
+	flex-wrap: wrap;
+	margin-bottom: 40px;
+`
+
+const S = {
+	Wrapper,
+	Title,
+	Form,
+	Container,
+}

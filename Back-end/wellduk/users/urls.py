934288227630a8af7064067eba616b32ed48d46e
gardening-y <60008786@naver.com>
2023-08-23 22:32:38 +0900

    fix: requirements수정

--- Back-end/wellduk/users/urls.py
+++ Back-end/wellduk/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include, re_path
 from .views import *
+
 from dj_rest_auth.registration.views import VerifyEmailView
 from dj_rest_auth.registration.views import RegisterView
 from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
@@ -9,7 +10,7 @@ urlpatterns = [
     path("register/", RegisterView.as_view(), name="rest_register"),
     path("login/", LoginView.as_view(), name="rest_login"),
     path("logout/", LogoutView.as_view(), name="rest_logout"),
-    path("user/", UserDetailsView.as_view(), name="rest_user_details"),
+    # path("user/", UserDetailsView.as_view(), name="rest_user_details"),
     # 일반 회원 회원가입/로그인
     # path('dj-rest-auth/', include('dj_rest_auth.urls')),
     # path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
